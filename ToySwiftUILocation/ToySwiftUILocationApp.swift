02f052e776479ae2d7760b62dfabd60872192e6d
Faiz Mokhtar <mfmokhtar@gmail.com>
2020-11-17 18:09:52 +0800

    Initial Commit

---
+++ ToySwiftUILocation/ToySwiftUILocationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToySwiftUILocationApp.swift
+//  ToySwiftUILocation
+//
+//  Created by Faiz Mokhtar AD0502 on 17/11/2020.
+//
+
+import SwiftUI
+
+@main
+struct ToySwiftUILocationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
